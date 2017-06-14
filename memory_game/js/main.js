9dbb0eca1313d043964d5fd6745550706a23d3b6
Nathaniel Hodges <nhodges3@gsu.edu>
2017-06-13 23:48:04 -0400

    !

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,63 @@
 console.log("Up and running!");
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage:	"images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage:	"images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage:	"images/king-of-diamonds.png",
+	},
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour ="king"
+];
 
-console.log("User flipped " + cardOne)
+var cardsInPlay = [];
+
+
+//var cardThree = cards[3];
+//var cardFour = cards[4];
+
+function checkForMatch(argument) {
+	if (cardsInPlay[0].rank === cardsInPlay[1].rank && cardsInPlay[0].cardImage !== cardsInPlay[1].cardImage) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
+}
+
+
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	if (cardsInPlay.length === 2) {
+    // If so, call the checkForMatch function
+    checkForMatch();
+  }
+}
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement)
+
+	}
+}
+
+console.log(flipCard(0));
+console.log(flipCard(1));
